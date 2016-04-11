455852a40662a355f565347aec6b109bf9cb157a
xiaoqinchen <chenxiaoqin47@gmail.com>
2016-04-11 19:11:27 -0400

    Added files via upload

---
+++ quadratic.cpp
@@ -0,0 +1,61 @@
+//
+//  main.cpp
+//  Algebra solver
+//
+//  Created by Xiaoqin Chen on 16/4/11.
+//  Copyright © 2016年 Xiaoqin Chen. All rights reserved.
+//
+
+#include <iostream>
+#include<cmath>
+using namespace std;
+class quadratic
+    {
+private:
+    float a;
+    float b;
+    float c;
+    float theta;
+    double x1,x2;
+public:
+        quadratic(float a,float b,float c):a(a),b(b),c(c)
+        {
+            theta=b*b-4*a*c;
+        }
+        ~quadratic(){}
+        bool findRoot(quadratic quad)
+        {
+            bool find=true;
+            if(quad.theta<0)
+                find=false;
+            return find;
+            
+        }
+        void root(quadratic quad)
+        {
+            if(quad.a!=0){
+            quad.x1=(-quad.b+sqrt(quad.theta))/(2*quad.a);
+            quad.x2=(-quad.b-sqrt(quad.theta))/(2*quad.a);
+            cout<<"x1="<<quad.x1<<'\n';
+                cout<<"x2="<<quad.x2<<'\n';
+            }
+            else
+                cout<<"x="<<-c/b<<'\n';
+        }
+    };
+    
+
+int main(int argc, const char * argv[]) {
+    float a,b,c;
+    cout<<"ax^2+bx+c=0"<<'\n';
+    cout<<"please enter the exactly value of a,b,c:";
+    cin>>a;
+    cin>>b;
+    cin>>c;
+    quadratic quad(a,b,c);
+    if(quad.findRoot(quad))
+        quad.root(quad);
+    else
+        cout<<"can't find the root suit for this equcation!!!";
+    
+}
